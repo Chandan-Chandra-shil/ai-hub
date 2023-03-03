fc3147053df03fe3c2943632501217a677cbcfd8
Chandan-Chandra-shil <chandanchandrashil22@gmail.com>
2023-03-03 21:50:42 +0600

    added spinner and change some style

--- js/index.js
+++ js/index.js
@@ -7,40 +7,70 @@ const loadTools = async () => {
 
 const showTools = (tools) => {
   const toolsContainer = document.getElementById("tools-container");
-  // tools = tools.slice(0, 6);
+  
+  toolsContainer.innerHTML = "";
+  if (tools.length ) {
+    tools = tools.slice(0, 6);
+  }
+  else {
+    const showALL = document.getElementById('show-all').addEventListener('click', function () {
+      
+    
+  })
+  }
 
+  
+  
   tools.forEach((tool) => {
-    console.log(tool.published_in);
+    console.log();
     const div = document.createElement("col");
-
     div.classList.add("col");
     div.innerHTML = `
-    <div class="border p-3 rounded-2 ">
-    <img src="${tool.image}" class="card-img-top " alt="...">
-       <h5 class="card-title mb-1">Features: <br></h5>
-  <div class="card-body p-0 m-0">
-    <li> ${tool.features[0]}</li>
-    <li> ${tool.features[1]}</li>
-    <li> ${tool.features[2]}</li>
-    </div>
-    <hr>
-    <div class="d-flex justify-content-between">
-    <div>
-     <h5 class="card-title mb-1">${tool.name} <br></h5>
-      <h6 class="card-title mb-1">${tool.published_in} <br></h6>
-    </div>
-    <div id="" class=""><button>Details</button></div>
-       
+    <div class="border p-4 rounded-2 ">
+        <img src="${tool.image}" class="card-img-top " >
+       <h5 class="card-title mb-1 mt-2 font-semibold">Features <br></h5>
+      <div class="card-body p-0 m-0">
+        <ol>
+            <li>${tool.features[0]}</li>
+            <li>${tool.features[1]}</li>
+           <li>${tool.features[2] ? tool.features[2] : "No Found! "}</li>
+        </ol>
+      </div>
+         <hr>
+      <div class="d-flex justify-content-between">
+      <div>
+         <h5 class="card-title mb-1">${tool.name} <br></h5>
+        <h6 class="card-title mb-2">
+           <ion-icon name="calendar-clear-outline"></ion-icon> ${
+             tool.published_in
+           } <br>
+        </h6>
+      </div>
+      <div class="border  rounded-4 p-3 bg-danger-subtle">
+        <ion-icon name="arrow-forward-outline"></ion-icon>
+       </div>
+       </div>
+   </div>
+    
   
-    </div>
-     
-</div>
-   
-      
-
     `;
     toolsContainer.appendChild(div);
   });
+
+
+
 };
+
+
+const waitingSpinner = isLoading => {
+  const waitingYou = document.getElementById('waiting-you');
+  if (isLoading) {
+    waitingYou.classList.remove('d-none');
+  }
+  else {
+    waitingYou.classList.add('d-none');
+  }
+  
+}
 
 loadTools();
